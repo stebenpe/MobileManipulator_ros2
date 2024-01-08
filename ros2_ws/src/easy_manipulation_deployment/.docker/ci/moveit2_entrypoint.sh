fd2403e1c92ffaa91e66c90b0a19429cc25945c6
stebenpe <stevenpennock7@gmail.com>
2024-01-08 13:20:34 +0100

    cleanup

--- ros2_ws/src/easy_manipulation_deployment/.docker/ci/moveit2_entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup moveit2 environment
-source /opt/moveit2/install/setup.bash
-exec "$@"
--- ros2_ws/src/easy_perception_deployment/easy_perception_deployment/gui/scripts/kill.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-pkill showimage
-sudo docker stop epd_test_container -t 1
